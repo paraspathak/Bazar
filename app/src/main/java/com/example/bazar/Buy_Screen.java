8f999b229de1bf9b2125f32397f4f60b7094530c
Paras Pathak <paras.pathak@mavs.uta.edu>
2019-06-19 11:35:42 -0500

    Click handlers added

--- app/src/main/java/com/example/bazar/Buy_Screen.java
+++ app/src/main/java/com/example/bazar/Buy_Screen.java
@@ -4,11 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.TextView;
 
 public class Buy_Screen extends AppCompatActivity {
-    TextView test, short_description, number_of_item, price_total;
-    double price_of_item;
+    TextView test, short_description, number_of_item, price_total, success_screen;
+    double price_of_item, current_total;
     int quantity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class Buy_Screen extends AppCompatActivity {
         short_description = (TextView) findViewById(R.id.short_description_cart);
         number_of_item = (TextView) findViewById(R.id.number_to_buy);
         price_total = (TextView) findViewById(R.id.price_update);
+        success_screen = (TextView) findViewById(R.id.Success_banner);
         Bundle extras = getIntent().getExtras();
         number_of_item.addTextChangedListener(new TextWatcher() {
             @Override
@@ -28,12 +30,13 @@ public class Buy_Screen extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if(s.length()!=0){
-                    double temp = price_of_item *  Integer.parseInt(s.toString());
-                    price_total.setText(String.valueOf(temp));
+                    current_total = price_of_item *  Integer.parseInt(s.toString());
+                    price_total.setText(String.valueOf(current_total));
                 }
                 else {
                     //If 0 is selected put items price as 1
                     price_total.setText(String.valueOf(price_of_item));
+                    current_total = price_of_item;
                 }
             }
 
@@ -48,8 +51,16 @@ public class Buy_Screen extends AppCompatActivity {
             short_description.setText(extras.getString("description_short"));
             price_of_item=extras.getDouble("price");
             price_total.setText(String.valueOf(price_of_item));
+            current_total=price_of_item;
         }
 
 
+    }
+    public void add_to_cart(View view){
+        success_screen.setText("Item successfully added");
+    }
+
+    public void buy_click(View view){
+        success_screen.setText("Start a new intent of buying item");
     }
 }
--- app/src/main/java/com/example/bazar/Communicate_with_server.java
+++ app/src/main/java/com/example/bazar/Communicate_with_server.java
@@ -3,11 +3,10 @@ package com.example.bazar;
 import java.util.ArrayList;
 
 public class Communicate_with_server {
-    ArrayList<Items_on_sale> products_in_bazar;
-    ArrayList<Items_on_sale> my_products_in_bazar;
+    static ArrayList<Items_on_sale> products_in_bazar;
+    static ArrayList<Items_on_sale> my_products_in_bazar;
     public Communicate_with_server() {
-        ArrayList<Items_on_sale> products_in_bazar = new ArrayList<>();
-        ArrayList<Items_on_sale> my_products_in_bazar = new ArrayList<>();
+        products_in_bazar = new ArrayList<>();
         //Connect with server here
         products_in_bazar.add(new Items_on_sale(0,"Mouse",99.99,"Apple Mouse", "Awesome","/src/logo.png"));
         products_in_bazar.add(new Items_on_sale(1,"Keyboard",199.99,"Apple Keyboard","Awesome","/src/logo.png"));
@@ -18,12 +17,12 @@ public class Communicate_with_server {
     }
 
     static ArrayList<Items_on_sale> get_my_items_on_sale(){
-        ArrayList<Items_on_sale> my_products= new ArrayList<>();
+        ArrayList<Items_on_sale> my_products_in_bazar= new ArrayList<>();
         //Demo items currently
         //Need to load items from database here
-        my_products.add(new Items_on_sale(0,"Mouse",99.99,"Apple Mouse","Awesome","/src/logo.png"));
-        my_products.add(new Items_on_sale(1,"Keyboard",199.99,"Apple Keyboard","Awesome","/src/logo.png"));
-        return my_products;
+        my_products_in_bazar.add(new Items_on_sale(0,"Mouse",99.99,"Apple Mouse","Awesome","/src/logo.png"));
+        my_products_in_bazar.add(new Items_on_sale(1,"Keyboard",199.99,"Apple Keyboard","Awesome","/src/logo.png"));
+        return my_products_in_bazar;
     }
     static ArrayList<Items_on_sale> get_marketplace_items_on_sale(){
         ArrayList<Items_on_sale> products_on_bazar= new ArrayList<>();
@@ -38,11 +37,11 @@ public class Communicate_with_server {
 
         return products_on_bazar;
     }
-    Items_on_sale return_marketplace_at_id(int i){
+    static Items_on_sale return_marketplace_at_id(int i){
     //Connect to server here to get an id
-        for( Items_on_sale items : this.products_in_bazar){
+        for( Items_on_sale items : products_in_bazar){
             if(items.getProduct_id()==i){
-                return this.products_in_bazar.get(i);
+                return products_in_bazar.get(i);
             }
         }
         return null;
