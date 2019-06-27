8f5487c94c10eb3d56919b202bd61ef86488b7fe
Paras Pathak <paras.pathak@mavs.uta.edu>
2019-06-26 22:25:56 -0500

    Authentication successful to premade login credentials

--- app/src/main/java/com/example/bazar/CreateUser.java
+++ app/src/main/java/com/example/bazar/CreateUser.java
@@ -1,19 +1,27 @@
 package com.example.bazar;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Map;
 
 public class CreateUser extends AppCompatActivity {
 
     EditText username, password;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +38,7 @@ public class CreateUser extends AppCompatActivity {
             }
         });
 
+        mAuth = FirebaseAuth.getInstance();
 
         Button create = (Button) findViewById(R.id.new_user_create);
 
@@ -42,6 +51,19 @@ public class CreateUser extends AppCompatActivity {
                 String pword = password.getText().toString();
 
                 //Create a new User in database
+                mAuth.createUserWithEmailAndPassword(user,pword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                        else {
+
+                        }
+                    }
+                });
+
+
 
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 intent.putExtra("user",user);
--- app/src/main/java/com/example/bazar/MainActivity.java
+++ app/src/main/java/com/example/bazar/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.bazar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText Username;
     private EditText Password;
     private Button login_button;
-    private String username;
+    private String username, password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +33,13 @@ public class MainActivity extends AppCompatActivity {
         Username=(EditText) findViewById(R.id.username);
         Password = (EditText) findViewById(R.id.password);
         login_button = (Button) findViewById(R.id.loginbutton);
-        username = Username.getText().toString();
 
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        //Check if login credentials match or not
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //Create a new Account
         TextView new_account = (TextView) findViewById(R.id.create_new_account);
         new_account.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,32 +61,64 @@ public class MainActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if(extras!=null){
-            String user = extras.getString("user");
-            if(user!=null){
-                Username.setText(user);
+            username = extras.getString("user");
+            if(username!=null){
+                Username.setText(username);
             }
-            String pword = extras.getString("pword");
-            if(user!=null){
-                Password.setText(pword);
+            password = extras.getString("pword");
+            if(password!=null){
+                Password.setText(password);
             }
         }
+
+        FirebaseUser current_user = mAuth.getCurrentUser();
+        updateUI(current_user);
+
 
     }
 
     private void authenticate_user (String username, String password){
         //Needs to query database here for password and username
-        //Currently only implemented for admin admin
-        if(username.equals("admin") && password.equals("admin")){
-            Intent intent = new Intent(MainActivity.this, Slidermenu.class);
-            startActivity(intent);
-            intent.putExtra("username",this.username);
-        }
-        else{
-            Username.setText("Incorrect Password");
-        }
+
+        mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                    Toast.makeText(getApplicationContext(), "Authentication succeeded.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Slidermenu.class);
+                    //intent.putExtra("username",username);
+                    startActivity(intent);
+
+                }
+                else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    Username.setText("Incorrect Password");
+                }
+
+
+
+            }
+
+
+        });
     }
     public String return_username(){
         return username;
+    }
+    private void updateUI(FirebaseUser user) {
+        if(user!=null){
+            if(user.isEmailVerified()){
+                Username.setText((user.getEmail()));
+                Password.setText(user.getUid());
+                Intent intent = new Intent(MainActivity.this, Slidermenu.class);
+                intent.putExtra("username",this.username);
+                startActivity(intent);
+            }
+
+        }
     }
 
 }
